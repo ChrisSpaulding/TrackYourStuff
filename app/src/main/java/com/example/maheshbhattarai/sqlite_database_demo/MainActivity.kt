d9fdc9a70794af510f82a9dfe7fe46ef3f4f2221
jesus guzman <jguzman1835@gmail.com>
2018-03-15 11:55:50 -0600

    Proving my computer is not broken

--- app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/MainActivity.kt
+++ app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
+// Jesus can comment now.
 class MainActivity : AppCompatActivity() {
 
     lateinit var btn_androidmap: Button
