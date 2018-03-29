2305e2846dfe438f4cdcb88f2f6c39d973d1f773
Aguiardavidm <Aguiardavidm@gmail.com>
2018-03-29 17:38:50 -0600

    Adding dem unit tests bby

--- app/src/androidTest/java/com/example/trackYourStuff/sqlite_database_demo/DatabaseTesting.kt
+++ app/src/androidTest/java/com/example/trackYourStuff/sqlite_database_demo/DatabaseTesting.kt
@@ -1,68 +1,35 @@
 package com.example.trackYourStuff.sqlite_database_demo
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.Observer
-import android.arch.persistence.room.Room
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import android.test.MoreAsserts.assertEquals
 import com.example.trackYourStuff.sqlite_database_demo.database.AppDatabase
 import com.example.trackYourStuff.sqlite_database_demo.database.Registration
+import com.example.trackYourStuff.sqlite_database_demo.database.RegistrationDao
 import org.junit.After
-import org.junit.Before
 import org.junit.Test
+import org.junit.Before
 import org.junit.runner.RunWith
-import java.io.IOException
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-
 
 @RunWith(AndroidJUnit4::class)
 class DatabaseTesting {
-    protected lateinit var appDatabase: AppDatabase
+
+    private var registrationDao : RegistrationDao? = null
 
     @Before
-    fun initDb() {
-        appDatabase = Room.inMemoryDatabaseBuilder(
-                InstrumentationRegistry.getContext(),
-                AppDatabase::class.java)
-                .build()
+    fun createDB(){
+        AppDatabase.TEST_MODE = true
+        registrationDao = AppDatabase.getInstance(InstrumentationRegistry.getTargetContext())
+    }
+
+    @After
+    fun closeDb() {
+
     }
 
     @Test
-    fun insertEmployTest(){
-        val employee = Registration(employId = 0, firstName = "David",email = "AguiarDavidM@gmail.com",address = "123 Hello St",phone_number = "303-123-4567",password = "pass",role= "the boss")
-        val employeeID = appDatabase.employDao().insertEmploy(employee)
-        val employeeDB = appDatabase.employDao().findEmployByID(0)
-        assert(employeeDB.toString() == employee.toString())
-
-    }
-    /*@Test
-    fun insertUserTest() {
-        val user = User(userName = "Test User")
-        val userId = appDatabase.userDao().insertUser(user)
-        val userFromDb = appDatabase.userDao().user
-        assertEquals(userId, 1)
-        assertEquals(userFromDb?.userName, user.userName)
-    }*/
-
-    @After
-    @Throws(IOException::class)
-    fun closeDb() {
-        appDatabase.close()
-    }
-
-    @Throws(InterruptedException::class)
-    fun <T> LiveData<T>.getValueBlocking(): T? {
-        var value: T? = null
-        val latch = CountDownLatch(1)
-        val innerObserver = Observer<T> {
-            value = it
-            latch.countDown()
-        }
-        observeForever(innerObserver)
-        latch.await(2, TimeUnit.SECONDS)
-        return value
+    fun should_Insert_Registration(){
+        val registration = Registration(1,"David","Aguiardavidm@gmail.com","1234 Sesame Street","1234567891","password","boss")
+        registrationDao?.insertEmploy(registration)
     }
 
 
