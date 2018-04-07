952e26ea33a97b0387bd722f6a8a9466eb92bba4
Chris <ChrisSpaulding@gmail.com>
2018-04-07 13:27:10 -0600

    Text will be displayed on the screen, There is a location listener. I'm pushing because it builds and compiles and doesn't break and I will now be messing around and might break it.

--- app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/LocationLogger.kt
+++ app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/LocationLogger.kt
@@ -9,16 +9,70 @@ import android.widget.Button
 import android.content.SharedPreferences
 import android.R.id.edit
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.location.*
+import java.nio.file.Files.size
+import android.util.Log
+import android.widget.Toast
+import com.example.maheshbhattarai.sqlite_database_demo.R.layout.activity_locationlogger
+import java.io.IOException
+import java.util.*
+import android.location.LocationManager
+import android.provider.Settings.Secure
+import android.provider.Settings.Secure.isLocationProviderEnabled
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import com.example.maheshbhattarai.sqlite_database_demo.R.id.locationTextView
+import kotlinx.android.synthetic.main.activity_locationlogger.*
+
 
 class LocationLogger : AppCompatActivity() {
-
+    var permissionResults : Int=-1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_locationlogger)
 
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationListener = MyLocationListener()
+
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions( this,  arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),permissionResults )
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 5000, 10f, locationListener)
+        }
+
+        locationTextView.setText("Location: \n Long: ${locationListener.longitude} \n Lat: ${locationListener.latitude}")
+}
+
+
+
+
+
     }
 
+ class MyLocationListener:LocationListener {
+    var longitude : Double =0.0
+     var latitude : Double =0.0
+override fun onLocationChanged(loc: Location) {
+longitude = loc.longitude
+Log.v(TAG, longitude.toString())
+latitude = loc.latitude
+Log.v(TAG, latitude.toString())
 
 
 
-    }
+}
+
+override fun onProviderDisabled(provider:String) {}
+
+override fun onProviderEnabled(provider:String) {}
+
+override fun onStatusChanged(provider:String, status:Int, extras:Bundle) {}
+}
