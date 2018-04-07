5f43c91345547fa2c452e61cec7ed27d990df035
Chris <ChrisSpaulding@gmail.com>
2018-04-07 12:10:51 -0600

    LocationLogger is now an activity

--- app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/LocationLogger.kt
+++ app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/LocationLogger.kt
@@ -1,4 +1,15 @@
 package com.example.maheshbhattarai.sqlite_database_demo
 
-class LocationLogger {
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.SharedPreferences
+import android.R.id.edit
+import android.annotation.SuppressLint
+
+class LocationLogger : AppCompatActivity() {
+    
 }
