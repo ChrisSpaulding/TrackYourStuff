49d4fc1e3b5e87dbce8dfc42e4a84d137f91ec56
Chris <ChrisSpaulding@gmail.com>
2018-04-08 12:12:33 -0600

    Current GPS location sucsessfully displayed on screen in Location Logger.

--- app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/LocationLogger.kt
+++ app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/LocationLogger.kt
@@ -1,35 +1,35 @@
 package com.example.maheshbhattarai.sqlite_database_demo
 
-import android.content.Context
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.content.SharedPreferences
-import android.R.id.edit
-import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
+import android.content.*
 import android.location.*
-import java.nio.file.Files.size
 import android.util.Log
 import android.widget.Toast
-import com.example.maheshbhattarai.sqlite_database_demo.R.layout.activity_locationlogger
-import java.io.IOException
-import java.util.*
-import android.location.LocationManager
-import android.provider.Settings.Secure
-import android.provider.Settings.Secure.isLocationProviderEnabled
-import android.content.ContentResolver
-import android.content.pm.PackageManager
-import android.provider.Settings
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import com.example.maheshbhattarai.sqlite_database_demo.R.id.locationTextView
+import android.os.Looper
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import kotlinx.android.synthetic.main.activity_locationlogger.*
-const val PERMISSION_REQUEST_GPS = 0
 
 class LocationLogger : AppCompatActivity() {
+
+    lateinit var CurrentlatLng: LatLng
+    lateinit var mLocationRequest: LocationRequest
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    lateinit var mLocationCallback: LocationCallback
+    lateinit var mSettingsClient: SettingsClient
+    lateinit var mLocationSettingsRequest: LocationSettingsRequest
+    private val REQUEST_CHECK_SETTINGS = 0x1
+    lateinit var location: Location
+    val REQUEST_LOCATION = 1011
+    var currentLatitude: Double = 0.0
+    var currentLongitude: Double = 0.0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,40 +43,95 @@ class LocationLogger : AppCompatActivity() {
 }
 
 
+
 fun displayGPSLocation(){
-    val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-    val locationListener = MyLocationListener()
+    initLocation()
 
-    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+}
 
-        ActivityCompat.requestPermissions( this,  arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),PERMISSION_REQUEST_GPS )
+    private fun initLocation() {
+        try {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this@LocationLogger)
+            getLastLocation()
+            try {
 
-        locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, 5000, 10f, locationListener)
+                mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                        .addOnSuccessListener(this, object : OnSuccessListener<LocationSettingsResponse> {
+                            override fun onSuccess(p0: LocationSettingsResponse?) {
+                                mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                        mLocationCallback, Looper.myLooper());
+                            }
+
+                        }).addOnFailureListener(this, object : OnFailureListener {
+                            override fun onFailure(p0: java.lang.Exception) {
+                                val statusCode = (p0 as ApiException).getStatusCode();
+                                when (statusCode) {
+                                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                                        Log.i("Location", "Location settings are not satisfied. Attempting to upgrade " +
+                                                "location settings ");
+                                        try {
+                                            // Show the dialog by calling startResolutionForResult(), and check the
+                                            // result in onActivityResult().
+                                            val rae = p0 as ResolvableApiException
+                                            rae.startResolutionForResult(this@LocationLogger, REQUEST_CHECK_SETTINGS);
+                                        } catch (sie: IntentSender.SendIntentException) {
+                                            Log.i("Location", "PendingIntent unable to execute request.");
+                                        }
+                                    }
+
+                                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE ->
+                                        Toast.makeText(this@LocationLogger, "Location settings are inadequate, and cannot be \"+\n" +
+                                                "                                    \"fixed here. Fix in Settings.", Toast.LENGTH_LONG).show();
+
+
+                                }
+                            }
+
+                        })
+
+            } catch (unlikely: SecurityException) {
+                Log.e("Location", "Lost location permission. Could not request updates. " + unlikely)
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
-    var locationText :String = "Location: \n Long: ${locationListener.longitude} \n Lat: ${locationListener.latitude}"
-    locationTextView.text= locationText
-}
 
+    private fun getLastLocation() {
+        try {
+            mFusedLocationClient.getLastLocation()?.addOnCompleteListener { task ->
+                if (task.isSuccessful && task.result != null) {
+                    location = task.getResult()
+                    CurrentlatLng = LatLng(location.latitude, location.longitude)
+                    Log.e("CurrentLatLong2", "lat " + location.latitude + " long " + location.longitude)
+                    currentLatitude = location.latitude
+                    Log.i("Lat**", "$currentLatitude")
+                    currentLongitude = location.longitude
+                    Log.i("long **", "$currentLongitude")
+                    displayValues(location.longitude, location.latitude)
+
+
+                } else {
+                    Log.w("Location", "Failed to get location.")
+                }
+            }
+        } catch (unlikely: SecurityException) {
+            Log.e("Location", "Lost location permission." + unlikely)
+        }
 
     }
 
- class MyLocationListener:LocationListener {
-    var longitude : Double =0.0
-     var latitude : Double =0.0
-override fun onLocationChanged(loc: Location) {
-longitude = loc.longitude
-Log.v(TAG, longitude.toString())
-latitude = loc.latitude
-Log.v(TAG, latitude.toString())
 
-
+    fun displayValues(long : Double, lat :Double){
+        val longTxt = "Logitude: $long"
+        val latTxt = "Latitude: $lat"
+        locationTextView.text= "Location:"
+        longTxtView.text= longTxt
+        latTxtView.text=latTxt
+    }
 
 }
 
-override fun onProviderDisabled(provider:String) {}
 
-override fun onProviderEnabled(provider:String) {}
-
-override fun onStatusChanged(provider:String, status:Int, extras:Bundle) {}
-}
