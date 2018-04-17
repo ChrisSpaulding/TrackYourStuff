98d0bcec1d087d3514cc702d1c7be0152db9e3a5
ChrisSpaulding1991@gmail.com <ChrisSpaulding1991@gmail.com>
2018-04-17 16:48:51 -0600

    Should store location when get location button is pressed

--- app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/database/LocationDao.kt
+++ app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/database/LocationDao.kt
@@ -2,11 +2,10 @@ package com.example.maheshbhattarai.sqlite_database_demo.database
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
-import android.arch.persistence.room.OnConflictStrategy.IGNORE
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
 import android.arch.persistence.room.Update
-import com.example.maheshbhattarai.sqlite_database_demo.CustomDate
+import java.util.*
 
 @Dao
 interface LocationDao {
@@ -24,6 +23,6 @@ interface LocationDao {
     fun findAllEmploySync(): List<Registration>
 
     @Query("SELECT * FROM Location where date = Date")
-    fun findLocation(Date : CustomDate)
+    fun findLocation(Date : Date)
 
 }
