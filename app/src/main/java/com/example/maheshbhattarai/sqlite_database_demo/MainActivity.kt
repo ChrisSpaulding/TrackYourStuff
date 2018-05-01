12fd8659f1f57dc23af496f4ce88470d51c8113c
ChrisSpaulding1991@gmail.com <ChrisSpaulding1991@gmail.com>
2018-05-01 17:07:06 -0600

    removed sql button

--- app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/MainActivity.kt
+++ app/src/main/java/com/example/maheshbhattarai/sqlite_database_demo/MainActivity.kt
@@ -25,6 +25,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         })
 
+        btn_androidsqlite.visibility = View.GONE
+        btn_androidsqlite.setOnClickListener({
+             val intent = Intent(this,HomeActivity::class.java)
+             startActivity(intent)
+        })
+
         btn_location.setOnClickListener{
             val intent = Intent ( this, LocationLogger::class.java)
             startActivity(intent)
